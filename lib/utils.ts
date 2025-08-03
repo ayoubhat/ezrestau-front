e241c7b23acfcbaf7c7f5442bda62f1cd1846fe0
ayoubhatmi <ayoub.hatmi18@gmail.com>
2025-08-03 02:24:12 +0200

    Change domain name to .fr

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const rootDomain =
-  process.env.NODE_ENV === "production" ? "ezrestau.dev" : "localhost:3000";
+  process.env.NODE_ENV === "production" ? "ezrestau.fr" : "localhost:3000";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
